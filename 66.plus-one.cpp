980711e0ae60076589f6cfd4c6585ccade7e4885
hongjun <a0912427247@gmail.com>
2025-03-21 00:32:07 +0800

    新增 LeetCode 題目解法：1. Two Sum 與 66. Plus One

---
+++ 66.plus-one.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=66 lang=cpp
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size() - 1]+=1;
+        for(int i=digits.size()-1; i>=0; i--){
+            
+            if(digits[i]==10){
+                if(i!=0){
+                    digits[i]=0;
+                    digits[i-1]+=1;
+                }
+                else{
+                    digits[i]=0;
+                    digits.insert(digits.begin(), 1);
+                }
+
+            }
+            
+        }
+
+        return digits;
+    }
+};
+// @lc code=end
+
