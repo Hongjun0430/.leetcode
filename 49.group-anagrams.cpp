818a9082ef52e757a04ade01fdc90a658bec77f1
hongjun <a0912427247@gmail.com>
2025-04-10 13:13:04 +0800

    add 49.

--- 49.group-anagrams.cpp
+++ 49.group-anagrams.cpp
@@ -5,11 +5,26 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        unordered_map<string, vector<string>> map;
+        for (string s : strs)
+        {
+            string sorted = s;
+            sort(sorted.begin(), sorted.end());
+            map[sorted].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for (auto &pair : map)
+        {
+            ans.push_back(pair.second);
+        }
+
+        return ans;
     }
 };
 // @lc code=end
-
