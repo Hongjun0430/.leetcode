bdd074cc6a5ad0904cb85268edd201d05f8f7282
hongjun <a0912427247@gmail.com>
2025-04-10 15:38:31 +0800

    add 347.

--- 347.top-k-frequent-elements.cpp
+++ 347.top-k-frequent-elements.cpp
@@ -5,11 +5,31 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        vector<int> ans;
+        unordered_map<int, int> m;
+        for (int n : nums)
+        {
+            m[n]++;
+        }
+        vector<pair<int, int>> p;
+        for (auto &pair : m)
+        {
+            p.push_back({pair.second, pair.first});
+        }
+
+        sort(p.rbegin(), p.rend());
+
+        for (int i = 0; i < k; i++)
+        {
+            ans.push_back(p[i].second);
+        }
+
+        return ans;
     }
 };
 // @lc code=end
-
