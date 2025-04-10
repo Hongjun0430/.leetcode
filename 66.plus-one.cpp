dfdf7101db783146f8a6eeb4fad50af085658653
hongjun <a0912427247@gmail.com>
2025-04-10 15:00:28 +0800

    整理

--- 66.plus-one.cpp
+++ 66.plus-one.cpp
@@ -5,28 +5,31 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        digits[digits.size() - 1]+=1;
-        for(int i=digits.size()-1; i>=0; i--){
-            
-            if(digits[i]==10){
-                if(i!=0){
-                    digits[i]=0;
-                    digits[i-1]+=1;
+    vector<int> plusOne(vector<int> &digits)
+    {
+        digits[digits.size() - 1] += 1;
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+
+            if (digits[i] == 10)
+            {
+                if (i != 0)
+                {
+                    digits[i] = 0;
+                    digits[i - 1] += 1;
                 }
-                else{
-                    digits[i]=0;
+                else
+                {
+                    digits[i] = 0;
                     digits.insert(digits.begin(), 1);
                 }
-
             }
-            
         }
 
         return digits;
     }
 };
 // @lc code=end
-
