9c305129ae735a06b48126cd1cd657a0cff7131b
hongjun <a0912427247@gmail.com>
2025-04-11 17:58:36 +0800

    add 167.

--- 167.two-sum-ii-input-array-is-sorted.cpp
+++ 167.two-sum-ii-input-array-is-sorted.cpp
@@ -5,23 +5,28 @@
  */
 
 // @lc code=start
-class Solution {
-    public:
-        vector<int> twoSum(vector<int>& numbers, int target) {
-            int left=0,right=numbers.size()-1;
-            while(1){
-                int sum=numbers[left]+numbers[right];
-                if(sum==target){
-                    return {left+1,right+1};
-                }
-                else if(sum>target){
-                    right-=1;
-                }
-                else{
-                    left+=1;
-                }
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        int left = 0, right = numbers.size() - 1;
+        while (1)
+        {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target)
+            {
+                return {left + 1, right + 1};
+            }
+            else if (sum > target)
+            {
+                right -= 1;
+            }
+            else
+            {
+                left += 1;
             }
         }
+    }
 };
 // @lc code=end
-
