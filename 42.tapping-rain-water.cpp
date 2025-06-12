db136b70cb23f9517d85105986e0f770c9ad46b0
hongjun <a0912427247@gmail.com>
2025-06-13 00:36:45 +0800

    add 42.

--- 42.tapping-rain-water.cpp
+++ 42.tapping-rain-water.cpp
@@ -8,23 +8,24 @@ class Solution {
     public:
         int trap(vector<int>& height) {
             int ans = 0;
-            for (int i = 1; i < height.size() - 1; i++) {
-                int left = 0, right = height.size() - 1;
-                int leftMax = 0, rightMax = 0;
-                while (left < i) {
-                    if (height[left] > leftMax) {
+            int left = 0, right = height.size() - 1;
+            int leftMax = 0, rightMax = 0;
+
+            while (left <= right) {
+                if (height[left] <= height[right]) {
+                    if (height[left] >= leftMax) {
                         leftMax = height[left];
+                    } else {
+                        ans += leftMax - height[left];
                     }
                     left++;
-                }
-                while (right > i) {
-                    if (height[right] > rightMax) {
+                } else {
+                    if (height[right] >= rightMax) {
                         rightMax = height[right];
+                    } else {
+                        ans += rightMax - height[right];
                     }
                     right--;
-                }
-                if ((min(rightMax, leftMax) - height[i]) > 0) {
-                    ans += min(rightMax, leftMax) - height[i];
                 }
             }
 
